bb4983628931d180cdcd8e1f10d65a31094c690f
obrhubr <obrhubr@gmail.com>
2025-04-25 18:12:27 +0200

    refactor MCTS and make it a subclass of Maximiser

--- takeiteasy/mcts.py
+++ takeiteasy/mcts.py
@@ -5,7 +5,11 @@ import random
 from board import Board, N_TILES
 
 class ActionNode:
-	def __init__(self, board, piece, parent=None, exploration=1):
+	"""
+	ActionNodes represent tile placement steps in the MCTS graph.
+	At this node, the predetermined piece is placed onto the board for every unfilled tile.
+	"""
+	def __init__(self, board: Board, piece: tuple[int, int, int], parent: "RandomNode" = None, exploration: float = 1):
 		self.children = {}
 		self.parent = parent
 
@@ -15,19 +19,34 @@ class ActionNode:
 		self.board = board
 		self.piece = piece
 		self.exploration = exploration
+
+		# Keep track of which tiles were not yet explored
 		self.unfilled_tiles = [i for i in range(N_TILES) if i not in self.board.filled_tiles]
-		return
 	
-	def is_fully_expanded(self):
+	def is_fully_expanded(self) -> bool:
+		"""
+		Return if a child node was generated for each open tile.
+		For the first ActionNode, this would be 19 child RandomNodes.
+		"""
 		return len(self.children) >= (N_TILES - len(self.board.filled_tiles))
 	
-	def is_terminal(self):
+	def is_terminal(self) -> bool:
+		"""
+		Return True if the board is filled.
+		"""
 		return len(self.board.filled_tiles) == N_TILES
 	
-	def get_action(self):
+	def get_action(self) -> int:
+		"""
+		Get first unfilled tile_idx from open tiles and remove from queue.
+		"""
 		return self.unfilled_tiles.pop(0)
 	
-	def best_child(self, exploration=None):
+	def best_child(self, exploration: float | None = None) -> "RandomNode":
+		"""
+		Return the best child RandomNode according to UCT metric.
+		Maximise balance between exploration and exploitation.
+		"""
 		def uct(total_reward, visits, parent_visits, c=1):
 			return total_reward / visits + c * np.sqrt(np.log(parent_visits) / visits)
 		
@@ -37,20 +56,24 @@ class ActionNode:
 		if len(self.children) == 0:
 			raise Exception("Cannot get best child of unexpanded node.")
 		
+		# Custom key function for max
 		return max(
 			self.children.values(), 
 			key=lambda child: uct(child.reward, child.visits, self.visits, c=exploration)
 		)
 	
-	def best_action(self):
+	def best_action(self) -> "RandomNode":
 		"""
 		Pick the child with the highest visit count, then with the highest reward.
-		This is maximum exploitation used after exploring the nodes to pick the best action.
+		This function should be called after exploring the nodes to pick the best action.
 		"""
 		return max(self.children.values(), key=lambda c: (c.visits, c.reward))
 
 class RandomNode:
-	def __init__(self, board, tile_idx, parent=None):
+	"""
+	RandomNodes represent the step of drawing a random piece from the stack.
+	"""
+	def __init__(self, board: Board, tile_idx: int, parent: ActionNode = None):
 		self.children = {}
 		self.parent = parent
 
@@ -60,52 +83,60 @@ class RandomNode:
 		self.board = board
 		self.tile_idx = tile_idx
 		self.unplaced_pieces = board.pieces[:]
-		return
 	
-	def is_fully_expanded(self):
+	def is_fully_expanded(self) -> bool:
+		"""
+		Return True, if a childnode was created for all possible pieces left on the stack.
+		"""
 		return len(self.children) >= len(self.board.pieces)
 	
-	def is_terminal(self):
+	def is_terminal(self) -> bool:
+		"""
+		Return true if all tiles were filled.
+		"""
 		return len(self.board.filled_tiles) == N_TILES
 	
-	def get_piece(self):
+	def get_piece(self) -> tuple[int, int, int]:
+		"""
+		Return an unplaced piece and remove from stack.
+		"""
 		return self.unplaced_pieces.pop()
 	
-	def random_outcome(self):
+	def random_outcome(self) -> ActionNode:
+		"""
+		Return a random child, to simulate random piece picking process.
+		"""
 		return self.children[random.choice(list(self.children.keys()))]
 	
-	def get_child(self, piece, return_none=False):
+	def get_child(self, piece: tuple[int, int, int]) -> ActionNode:
 		"""
 		Get a child node by the piece placed.
 		"""
 		if piece in self.children:
 			return self.children[piece]
 		
-		# Return None instead of new node
-		if return_none:
-			return None
-		
 		# If the course of action wasn't in the explored states
 		return ActionNode(self.board, piece=piece)
 
-class MCTS:
-	def __init__(self, board, expansions=500, exploration=1, lookup=None, debug=False):
+class MCTSMaximiser:
+	"""
+	Monte-Carlo-Tree-Search implemented for take-it-easy.
+	"""
+	def __init__(self, board: Board, expansions: int = 500, exploration: float = 1, debug: bool = False):
 		self.board = board
 		self.debug = debug
 
-		# Store loaded lookup table
-		self.lookup = lookup
-
-		self.EXPANSIONS = expansions
+		# Parameters
+		self.expansions = expansions
 		self.exploration = exploration
 
 		# Cached Random node corresponding the last taken action
 		self.rootnode = None
 		return
 	
-	def simulate(self, board):
+	def simulate(self, board) -> int:
 		"""
-		Perform random rollouts.
+		Perform random rollouts and return the score
 		"""
 		unfilled_tiles = [i for i in range(N_TILES) if i not in self.board.filled_tiles]
 		random.shuffle(unfilled_tiles)
@@ -113,19 +144,20 @@ class MCTS:
 		for _ in range(N_TILES - len(board.filled_tiles)):
 			piece = board.pieces.pop()
 			board.play(piece, unfilled_tiles.pop(0))
-		reward = board.score()
 
-		return reward
+		return board.score()
 	
-	def run(self, piece: int) -> int:
+	def best_move(self, piece: tuple[int, int, int]) -> tuple[int, dict[int, float], ActionNode | RandomNode]:
 		"""
 		Return the best move to make with the given piece on the current board.
-		Use cached MCTS from previous turns if available.
+		Uses cached MCTS graph from previous turns.
+
+		Overrides the default maximiser `best_move` function.
 		"""
 		# Use previously explored (cached) rootnode if it exists
 		rootnode = ActionNode(self.board, piece=piece) if self.rootnode is None else self.rootnode.get_child(piece)
 
-		for _ in tqdm(range(self.EXPANSIONS)) if self.debug else range(self.EXPANSIONS):
+		for _ in tqdm(range(self.expansions)) if self.debug else range(self.expansions):
 			node = rootnode
 
 			# Selection
@@ -138,6 +170,7 @@ class MCTS:
 			# Expansion
 			if not node.is_terminal():
 				if type(node) is ActionNode:
+					# Action node: set piece on not yet explored tile
 					tile_idx = node.get_action()
 
 					new_board = node.board.clone()
@@ -147,6 +180,7 @@ class MCTS:
 					node.children[tile_idx] = outcome
 					node = outcome
 				else:
+					# Random node: Get random piece from pieces left that were not yet simulated
 					piece = node.get_piece()
 					outcome = ActionNode(node.board.clone(), piece, parent=node, exploration=self.exploration)
 					node.children[piece] = outcome
@@ -174,36 +208,15 @@ class MCTS:
 
 		return best_action.tile_idx, tile_values, rootnode
 
-	def play_game(self) -> int:
-		"""
-		Play a single game and return the score.
-
-		If a lookup table was loaded, use it.
-		"""
-		for _ in range(N_TILES - 1):
-			piece = self.board.draw()
-
-			# Use lookup table
-			key = (piece, str(self.board.board))
-			if self.lookup is not None and key in self.lookup:
-				idx = self.lookup[key]
-			else:
-				idx, _, _ = self.run(piece)
-
-			self.board.play(piece, idx)
-
-		return self.board.score()
-
 if __name__ == "__main__":
 	board = Board()
-	solver = MCTS(board, debug=True, expansions=100000)
+	solver = MCTSMaximiser(board, debug=True, expansions=100000)
 	
 	for _ in range(N_TILES):
 		piece = solver.board.draw()
-		idx, tile_values, _ = solver.run(piece)
-		
-		solver.board.show(tile_values=tile_values, piece=piece)
+		idx, tile_values, _ = solver.best_move(piece)
 
+		solver.board.show(tile_values=tile_values, piece=piece)
 		solver.board.play(piece, idx)
 		
 		# Wait for confirmation
