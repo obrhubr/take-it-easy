d5178b9b8bb287792512b110d15e8e56d1390085
obrhubr <obrhubr@gmail.com>
2025-04-28 16:23:02 +0200

    force reinstall wheel in makefile

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 cd bindings
 maturin build -r
 
-pip install target/wheels/rust_takeiteasy-0.1.0-cp311-cp311-manylinux_2_34_x86_64.whl
+pip install target/wheels/rust_takeiteasy-0.1.0-cp311-cp311-manylinux_2_34_x86_64.whl --force
