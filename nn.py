2b987f594e0a51554fd0b16feb4178d0ac9d11c6
obrhubr <obrhubr@gmail.com>
2025-05-03 14:09:23 +0200

    update benchmark script and NNMaximiser implementation

--- nn.py
+++ nn.py
@@ -281,8 +281,10 @@ class NNMaximiser(Maximiser):
 		super().__init__(board, debug)
 
 		self.net = Network()
-		self.net.load()
+		self.net.load(device="cpu")
+		self.net.net.eval()
 
+	@torch.no_grad()
 	def best_move(self, piece: tuple[int, int, int]) -> tuple[int, list[int]]:
 		"""
 		Use the nn to get an expected score for the current board.
@@ -290,6 +292,7 @@ class NNMaximiser(Maximiser):
 		states = torch.zeros((len(self.board.empty_tiles), self.net.input_size), dtype=torch.float)
 		rewards = torch.zeros((len(self.board.empty_tiles),), dtype=torch.float)
 
+		tile_labels = {}
 		for idx, tile in enumerate(self.board.empty_tiles):
 			self.board.board[tile] = piece
 			states[idx] = torch.from_numpy(self.board.one_hot()).float()
@@ -298,11 +301,20 @@ class NNMaximiser(Maximiser):
 
 		if len(self.board.empty_tiles) > 1:
 			qd = self.net.net(states)
-			best_action = (qd + rewards.unsqueeze(1)).mean(1).argmax()
+			expectations = (qd + rewards.unsqueeze(1)).mean(1)
+			best_action = expectations.argmax()
+			
+			if self.debug:
+				score = self.board.score()
+				tile_labels = {tile: expectations[n] + score for n, tile in enumerate(self.board.empty_tiles)}
 		else:
 			best_action = rewards.argmax()
+			
+			if self.debug:
+				score = self.board.score()
+				tile_labels = {tile: rewards[n] + score for n, tile in enumerate(self.board.empty_tiles)}
 
-		return self.board.empty_tiles[best_action], {}
+		return self.board.empty_tiles[best_action], tile_labels
 	
 if __name__ == "__main__":
 
